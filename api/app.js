719f32aeee6696ece141c82f4374d0461426a71f
Suissa <jnascimento@gmail.com>
2017-09-02 14:07:07 -0300

    Subindo modulo de User

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-const mongoose = require( './config' )
+require( './config' )
 
 const express = require( 'express' )
 const path = require( 'path' )
@@ -8,7 +8,7 @@ const cookieParser = require( 'cookie-parser' )
 const bodyParser = require( 'body-parser' )
 
 const index = require( './routes/index' )
-const users = require( './routes/users' )
+const users = require( './modules/User/routes' )
 
 const app = express()
 
@@ -25,7 +25,7 @@ app.use( cookieParser() )
 app.use( express.static( path.join( __dirname, 'public' ) ) )
 
 app.use( '/', index )
-app.use( '/users', users )
+app.use( '/api/users', users )
 
 // catch 404 and forward to error handler
 app.use( function ( req, res, next ) {
