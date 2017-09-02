5142fb9845f7949932a1796feabfc4c51fdc8771
Suissa <jnascimento@gmail.com>
2017-09-02 13:50:53 -0300

    Ateh o delete

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,5 +1,18 @@
 const Model = require( './../models/model' )
 
+const create = ( req, res ) => {
+
+  const body = req.body
+  console.log( '------------------------------------' )
+  console.log( 'body:', body )
+  console.log( '------------------------------------' )
+  Model.create( body, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
 const find = ( req, res, query = {} ) => 
   Model.find( query, ( err, data ) => {
     if ( err ) return console.log( 'ERRO: ', err )
@@ -7,6 +20,48 @@ const find = ( req, res, query = {} ) =>
     return res.json( data )
   } )
 
+const findOne = ( req, res ) => {
+
+  const query = {
+    _id: req.params.id
+  }
+
+  return Model.findOne( query, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
+const update = ( req, res ) => {
+  const query = {
+    _id: req.params.id
+  }
+  const mod = req.body
+  
+  Model.update( query, mod, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
+const remove = ( req, res ) => {
+  const query = {
+    _id: req.params.id
+  }
+  
+  Model.remove( query, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
 module.exports = {
-  find 
-} 
+  create,
+  find,
+  findOne,
+  update,
+  remove 
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -7,8 +7,20 @@ router.get( '/', ( req, res, next ) =>
   Controller.find( req, res, {} )
 )
 
+router.get( '/:id', ( req, res, next ) => 
+  Controller.findOne( req, res )
+)
+
 router.post( '/', ( req, res, next ) => 
   Controller.create( req, res )
+)
+
+router.put( '/:id', ( req, res, next ) => 
+  Controller.update( req, res )
+)
+
+router.delete( '/:id', ( req, res, next ) => 
+  Controller.remove( req, res )
 )
 
 module.exports = router
